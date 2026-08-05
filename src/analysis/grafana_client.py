753a4f535f125eb7ee88c7be78ee94feb09a6a8d
John Weigand <john.weigand@hashicorp.com>
2026-08-05 14:26:40 -0400

    change license to set public

--- src/analysis/grafana_client.py
+++ src/analysis/grafana_client.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2026 IBM Corporation
-# SPDX-License-Identifier: IPL-1.0
+# SPDX-License-Identifier: MPL-2.0
 
 """
 Grafana API Client for fetching metrics during load test analysis.
